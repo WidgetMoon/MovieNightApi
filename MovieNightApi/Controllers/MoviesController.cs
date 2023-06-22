52d9ff086978baaccabef2d4f2d99d9fc6f4e014
Adam Móricz <adam.moricz@gmail.com>
2023-06-22 15:37:40 +0200

    lets start this bad boy

---
+++ MovieNightApi/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieNightApi.Controllers
+{
+    [Route("api/v{version:apiVersion}/movies")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetMovie()
+        {
+            var movie = "Movie";
+            return Ok(movie);
+        }
+    }
+}
--- MovieNightApi/Program.cs
+++ MovieNightApi/Program.cs
@@ -2,10 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options=>
+{
+    options.ReturnHttpNotAcceptable = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(setupAction =>
+{
+    setupAction.AssumeDefaultVersionWhenUnspecified = true;
+    setupAction.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    setupAction.ReportApiVersions = true;
+});
 
 var app = builder.Build();
 
