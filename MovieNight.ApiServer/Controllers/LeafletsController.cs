47c4cef204a07194128f74e32efca7128f35b5bd
Adam Moricz <adam.moricz@gmail.com>
2024-05-16 23:08:21 +0200

    why no swagger

--- MovieNight.ApiServer/Controllers/LeafletsController.cs
+++ MovieNight.ApiServer/Controllers/LeafletsController.cs
@@ -4,6 +4,9 @@ using MovieNight.Core.Handlers.Interfaces;
 
 namespace MovieNight.ApiServer.Controllers
 {
+    /// <summary>
+    /// Endpoints for leaflets.
+    /// </summary>
     [Route("api/leaflets")]
     [ApiController]
     [Produces("application/json")]
@@ -11,6 +14,7 @@ namespace MovieNight.ApiServer.Controllers
     {
         private readonly IMovieHandler _movieHandler;
 
+        /// <inheritdoc />
         public LeafletsController(IMovieHandler movieHandler)
         {
             _movieHandler = movieHandler;
@@ -23,7 +27,7 @@ namespace MovieNight.ApiServer.Controllers
         /// <returns>Leaflets for products.</returns>
         /// <response code="200">Returns leaflets for products.</response>
         /// <response code="404">If the leaflets are not found.</response>
-        [HttpPost]
+        [HttpPost("products")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetLeaflets([FromBody] List<string> products)
